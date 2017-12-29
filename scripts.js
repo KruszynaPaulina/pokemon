6862d58a083fd1cbf167c9874cee74afc6b00340
Paulina Kruszyna <kruszyna.paulina@gmail.com>
2017-12-29 15:24:50 +0100

    Add pokemon

---
+++ scripts.js
@@ -0,0 +1,36 @@
+$( document ).ready(function() {
+    var url = "https://pokeapi.co/api/v2/pokemon/";
+
+	var tablePokemons = $(".tablePokemons");
+
+	function getPokemon() {
+		$.ajax({
+			method: 'GET',
+			url: url, 
+			success: showData
+		});
+	}
+
+	function showData(resp) {
+		var arrayPokemons = resp.results;
+		
+	    arrayPokemons.forEach(function showPokemonName(pokemon, i) {
+	    	i += 1;
+	    	tablePokemons.append("<tr class=" + i + "><td>" + pokemon.name + "</td></tr>");
+	    });
+	    
+	};
+
+	getPokemon()
+
+	function showPokemonImage() {
+		
+		for (var x=1; x<=20; x++) {
+		    $('body').append("<img src='https://pokeapi.co/media/img/" + x + ".png' alt='obrazek'>");
+		}
+	}
+
+	showPokemonImage();
+});
+
+
